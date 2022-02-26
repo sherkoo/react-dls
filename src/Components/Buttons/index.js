50a515d38a61cf63ddb356b0bd67bfd40e0eafff
Stephen <stephenherko@gmail.com>
2022-02-26 11:29:13 -0600

    Added click prop to buttons

--- src/Components/Buttons/index.js
+++ src/Components/Buttons/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ButtonStyle} from './button.style';
+import { ButtonStyle } from './button.style';
 
 /**
  * Props
@@ -12,14 +12,23 @@ import {ButtonStyle} from './button.style';
  * 
  * @param {props.type}
  * @returns primary, secondary, disabled, ghost, error, success
+ * 
+ * @param {props.click}
+ * @returns area for custom methods
  */
 
 function Button(props) {
   return (
-    <ButtonStyle size={props.size} type={props.type}>
+    <ButtonStyle onClick={props.click} size={props.size} type={props.type}>
       {props.label}
     </ButtonStyle>
   );
+}
+
+Button.defaultProps = {
+  label: "Default label",
+  size: "large",
+  type: "primary",
 }
 
 export default Button;
